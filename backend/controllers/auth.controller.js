2d43b52158a744165d10562e9c3b338a89838f8a
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-08-29 18:04:23 +0530

    verification code sent successfully

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,48 +1,53 @@
 import { User } from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js';
+import { sendVerificationEmail } from '../mailtrap/emails.js';
 
 export const signup = async (req, res) => {
-    const { email, password, name } = req.body;
-    try {
-        if (!email || !password || !name) {
-            throw new Error('All fields are required')
-        }
+	const { email, password, name } = req.body;
 
-        const userAlreadyExists = await User.findOne({ email });
-        console.log("userAlreadyExist", userAlreadyExists);
-        
-        if (userAlreadyExists) {
-            return res.status(400).json({ success: false, message: "User already exists" });
-        }
+	try {
+		if (!email || !password || !name) {
+			throw new Error("All fields are required");
+		}
 
-        const hashedPassword = await bcryptjs.hash(password, 10);
-        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+		const userAlreadyExists = await User.findOne({ email });
+		console.log("userAlreadyExists", userAlreadyExists);
 
-        const user = new User({
-            email,
-            password: hashedPassword,
-            name,
-            verificationToken,
-            verificationTokenExpiresAt: Date.now() + 24 * 60 * 1000
-        })
+		if (userAlreadyExists) {
+			return res.status(400).json({ success: false, message: "User already exists" });
+		}
 
-        await user.save();
+		const hashedPassword = await bcryptjs.hash(password, 10);
+		const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
 
+		const user = new User({
+			email,
+			password: hashedPassword,
+			name,
+			verificationToken,
+			verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 hours
+		});
 
-        generateTokenAndSetCookie(res, user._id);
-        res.status(201).json({
-            success: true,
-            message: "User created successfully",
-            user: {
-                ...user._doc,
-                password: undefined
-            }
-        })
-    } catch (error) {
-        res.status(400).json({ success: false, message: error.message });
-    }
-}
+		await user.save();
+
+		// jwt
+		generateTokenAndSetCookie(res, user._id);
+
+		await sendVerificationEmail(user.email, verificationToken);
+
+		res.status(201).json({
+			success: true,
+			message: "User created successfully",
+			user: {
+				...user._doc,
+				password: undefined,
+			},
+		});
+	} catch (error) {
+		res.status(400).json({ success: false, message: error.message });
+	}
+};
 
 export const login = async (req, res) => {
     res.send("login route");
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -8,4 +8,6 @@ router.post("/login", login )
 
 router.post("/logout", logout )
 
+router.post('/veri')
+
 export default router;
