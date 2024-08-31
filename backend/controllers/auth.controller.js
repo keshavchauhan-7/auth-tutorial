70009a8a5c477cf4135698898598dd540a8b3494
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-08-31 11:51:18 +0530

    Generate reset token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { User } from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import crypto from "crypto"
 import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js';
 import { sendVerificationEmail, sendWelcomeEmail } from '../mailtrap/emails.js';
 
@@ -66,6 +67,7 @@ export const verifyEmail = async (req, res) => {
 		user.verificationToken = undefined;
 		user.verificationTokenExpiresAt = undefined;
 		await user.save();
+
 		await sendWelcomeEmail(user.email, user.name);
 
 		res.status(200).json({
@@ -84,9 +86,56 @@ export const verifyEmail = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-	res.send("login route");
+	const { email, password } = req.body;
+	try {
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(400).json({ success: false, message: "Invalid credentials" });
+		}
+		const isPasswordValid = await bcryptjs.compare(password, user.password);
+		if (!isPasswordValid) {
+			return res.status(400).json({ success: false, message: "Invalid credentials" });
+		}
+		generateTokenAndSetCookie(res, user._id)
+
+		user.lastLogin = new Date();
+		await user.save();
+
+		res.status(200).json({
+			success: true,
+			message: "Logged in successfully",
+			user: {
+				...user._doc,
+				password: undefined,
+			}
+		})
+
+	} catch (error) {
+		console.log("Error in login", error)
+		res.status(400).json({ success: false, message: error.message });
+
+	}
 }
 
 export const logout = async (req, res) => {
-	res.send("logout route");
+	res.clearCookie("token")
+	res.status(200).json({ success: true, message: "Logged out successfully" });
+}
+
+export const forgotPassword = async (req, res) => {
+	const { email } = req.body;
+	try {
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			return res.status(400).json({ success: false, message: "User not found" });
+		}
+
+		//Generate reset token
+		  const resetToken = crypto.randomBytes(20).toString('hex');
+		  const resetTokenExpiresAt = Date.now() + 1 * 60 * 60 * 1000; // 1 hour
+
+	} catch (error) {
+
+	}
 }
