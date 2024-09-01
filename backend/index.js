f3850f838cf82ad042f2bba610e45e4a213b4bd6
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-09-01 22:49:55 +0530

    checkauth complete

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
 import { connectDB } from './db/connectDB.js';
+
 import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 
