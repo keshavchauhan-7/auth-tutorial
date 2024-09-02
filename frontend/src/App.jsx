a53449a49b64dbb6616344a7ca8fc059828ed4ed
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-09-02 17:27:17 +0530

    create email verification token

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import {Route, Routes} from 'react-router-dom';
 import FloatingShape from "./components/FloatingShape";
+
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
+import EmailVerificationPage from './pages/EmailVerificationPage';
 
 function App() {
 
@@ -15,6 +17,7 @@ function App() {
         <Route path='/' element={"Home"} />
         <Route path='/signup' element={<SignUpPage/>} />
         <Route path='/login' element={<LoginPage/>} />
+        <Route path='/verify-email' element={<EmailVerificationPage/>} />
       </Routes>
     </div>
   )
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,27 +1,27 @@
 import React, { useState } from 'react'
 import { motion } from "framer-motion";
 import Input from "../components/Input";
-import { Lock, Mail, User } from "lucide-react";
+import { Loader, Lock, Mail, User } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
-// import { useAuthStore } from "../store/authStore";
+import { useAuthStore } from '../store/authStore';
 
 const SignUpPage = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
 
-    // const { signup, error, isLoading } = useAuthStore();
+    const { signup, error, isLoading } = useAuthStore();
 
     const handleSignUp = async (e) => {
         e.preventDefault();
-        // try {
-        //     await signup(email, password, name);
-        //     navigate("/verify-email");
-        // } catch (error) {
-        //     console.log(error);
-        // }
+        try {
+            await signup(email, password, name);
+            navigate("/verify-email");
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     return (
@@ -59,7 +59,7 @@ const SignUpPage = () => {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                    {/* {error && <p className='text-red-500 font-semibold mt-2'>{error}</p>} */}
+                    {error && <p className='text-red-500 font-semibold mt-2'>{error}</p>}
                     <PasswordStrengthMeter password={password} />
 
                     <motion.button
@@ -70,10 +70,9 @@ const SignUpPage = () => {
                         whileHover={{ scale: 1.02 }}
                         whileTap={{ scale: 0.98 }}
                         type='submit'
-                        // disabled={isLoading}
+                        disabled={isLoading}
                     >
-                        Sign Up
-                        {/* {isLoading ? <Loader className=' animate-spin mx-auto' size={24} /> : "Sign Up"} */}
+                        {isLoading ? <Loader className=' animate-spin mx-auto' size={24} /> : "Sign Up"}
                     </motion.button>
                 </form>
             </div>
