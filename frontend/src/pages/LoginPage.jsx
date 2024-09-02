9cdfff2c7023208977fbdb78f4a0ff89f2c25065
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-09-03 00:15:52 +0530

    login implementation successfully

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,13 +3,13 @@ import { motion } from "framer-motion";
 import { Mail, Lock, Loader } from "lucide-react";
 import { Link } from "react-router-dom";
 import Input from "../components/Input";
-// import { useAuthStore } from "../store/authStore";
+import { useAuthStore } from "../store/authStore";
 
 const LoginPage = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    // const { login, isLoading, error } = useAuthStore();
+    const { login, isLoading, error } = useAuthStore();
 
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -50,17 +50,16 @@ const LoginPage = () => {
                             Forgot password?
                         </Link>
                     </div>
-                    {/* {error && <p className='text-red-500 font-semibold mb-2'>{error}</p>} */}
+                    {error && <p className='text-red-500 font-semibold mb-2'>{error}</p>}
 
                     <motion.button
                         whileHover={{ scale: 1.02 }}
                         whileTap={{ scale: 0.98 }}
                         className='w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200'
                         type='submit'
-                    // disabled={isLoading}
+                        disabled={isLoading}
                     >
-                        {/* {isLoading ? <Loader className='w-6 h-6 animate-spin  mx-auto' /> : "Login"} */}
-                        Login
+                        {isLoading ? <Loader className='w-6 h-6 animate-spin  mx-auto' /> : "Login"}
                     </motion.button>
                 </form>
             </div>
