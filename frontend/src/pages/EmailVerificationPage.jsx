2b23bfca8a20a21459219b10906093fde859ae21
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-09-03 00:08:46 +0530

    routes protected

--- frontend/src/pages/EmailVerificationPage.jsx
+++ frontend/src/pages/EmailVerificationPage.jsx
@@ -1,16 +1,16 @@
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-// import { useAuthStore } from "../store/authStore";
-// import toast from "react-hot-toast";
+import { useAuthStore } from "../store/authStore";
+import toast from "react-hot-toast";
 
 const EmailVerificationPage = () => {
     const [code, setCode] = useState(["", "", "", "", "", ""]);
     const inputRefs = useRef([]);
     const navigate = useNavigate();
-    const isLoading = false;
+    // const isLoading = false;
 
-    // const { error, isLoading, verifyEmail } = useAuthStore();
+    const { error, isLoading, verifyEmail} = useAuthStore();
 
     const handleChange = (index, value) => {
         const newCode = [...code];
@@ -48,14 +48,13 @@ const EmailVerificationPage = () => {
     const handleSubmit = async (e) => {
     	e.preventDefault();
     	const verificationCode = code.join("");
-        alert(`verification code submitted: ${verificationCode}`)
-    	// try {
-    	// 	await verifyEmail(verificationCode);
-    	// 	navigate("/");
-    	// 	toast.success("Email verified successfully");
-    	// } catch (error) {
-    	// 	console.log(error);
-    	// }
+    	try {
+    		await verifyEmail(verificationCode);
+    		navigate("/");
+    		toast.success("Email verified successfully");
+    	} catch (error) {
+    		console.log(error);
+    	}
     };
 
     // Auto submit when all fields are filled
@@ -93,7 +92,7 @@ const EmailVerificationPage = () => {
                             />
                         ))}
                     </div>
-                    {/* {error && <p className='text-red-500 font-semibold mt-2'>{error}</p>} */}
+                    {error && <p className='text-red-500 font-semibold mt-2'>{error}</p>}
                     <motion.button
 						whileHover={{ scale: 1.05 }}
 						whileTap={{ scale: 0.95 }}
